9699eb4aee75f2cccbe203079194fa2ed659b9ef
Jung Lee <jungyeobled@gmail.com>
2018-08-12 10:52:48 -0400

    final edit

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,7 +4,9 @@ import csv
 date = []
 revenue = []
 
-with open("budget_data.csv") as csvfile:
+budgetcsv = os.path.join('budget_data.csv')
+
+with open(budgetcsv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
     csv_header = next(csvfile)
 
@@ -40,5 +42,3 @@ with open(output_file, "w") as txt_file:
     txt_file.write(f"Greatest Increase in Revenue: {date[revChange.index(max(revChange))+1]} (${max(revChange)})")
     txt_file.write('\n')
     txt_file.write(f"Greatest Decrease in Revenue: {date[revChange.index(min(revChange))+1]} (${min(revChange)})\n")
-
-    
