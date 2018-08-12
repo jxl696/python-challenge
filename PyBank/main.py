2bb2e9f1ffa51086583eb43893e6b9d48067cd1b
Jung Lee <jungyeobled@gmail.com>
2018-08-12 09:38:15 -0400

    edit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,18 +1,18 @@
 import os
 import csv
 
-# Variables to track
+
 date = []
 revenue = []
 
-# Read Files
+
 with open("budget_data.csv") as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
-    # skip the header row.
+    
     csv_header = next(csvfile)
     for row in csvreader:
         date.append(row[0])
-        # read as string; convert to integer.
+        
         revenue.append(int(row[1]))
 print("Financial Analysis")
 print("----------------------------")
@@ -20,12 +20,10 @@ print(f"Total Months: {len(date)}")
 print(f"Total Revenue: ${sum(revenue)}")
 
 revChange = []
-# revenue change starts with 2nd value.
-# Loop through all the rows of data we collect
+
 for i in range(1, len(revenue)):
     revChange.append(revenue[i] - revenue[i-1])
 
 print(f"Average Revenue Change: ${sum(revChange)/len(revChange)}")
-# "date" list has 1 more value than "revCh" list.
 print(f"Greatest Increase in Revenue: {date[revChange.index(max(revChange))+1]} (${max(revChange)})")
 print(f"Greatest Decrease in Revenue: {date[revChange.index(min(revChange))+1]} (${min(revChange)})\n")
